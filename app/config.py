91cec022c4f164fb8529f29f25546df7445fa961
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-03-05 17:12:40 +0000

    chore(config) : add Jupyter Cells variables

--- app/config.py
+++ app/config.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 import tensorflow as tf
 import cv2
 
-# --- URL : postgresql+asyncpg://<user>:<password>@<host>:<port>/<db_name> 
 load_dotenv()
+
+# --- DB
+# URL : postgresql+asyncpg://<user>:<password>@<host>:<port>/<db_name> 
 
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
@@ -13,14 +15,23 @@ DB_NAME = os.getenv("DB_NAME")
 
 DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
-# --- load model CNN in memory 
-model = tf.keras.models.load_model('my_model_emotion_detection.keras')
-# tf_enable_onednn_opts=0
-# TF_ENABLE_ONEDNN_OPTS = 0
+# --- Jupyter Cellls
+data_dir = os.getenv("data_notebook_path")
+BATCH_SIZE = 32     # samples : 16, 32, 64, 128
+IMG_HEIGHT = 48     # DATASET size: 48 * 48
+IMG_WIDTH = 48
+IMG_SIZE=(IMG_WIDTH, IMG_HEIGHT)
+NUM_CLASSES = 7     # (angry, disgust, fear, happy, neutral, sad, surprise)
 
-# --- load model Haar Cascade
+
+
+# -> load model CNN in memory 
+model = tf.keras.models.load_model('my_model_emotion_detection.keras')
+
+
+# -> load model Haar Cascade
 name_xml_file = 'haarcascade-frontalface-default.xml'
 face_cascade = cv2.CascadeClassifier(name_xml_file)
 
-# --- Class Names
+# -> Class Names
 class_names = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
