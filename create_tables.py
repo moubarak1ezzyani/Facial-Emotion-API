e7a015bbfb4fb6e8c8fed123fe2008051a508da0
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-03-07 01:16:21 +0000

    move 'create_tables' to project root

---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import asyncio
+from dotenv import load_dotenv
+from app.database import Base, engine 
+
+async def init_db():
+    print("Connexion à la base de données...")
+    async with engine.begin() as conn:
+        print("Suppression des anciennes tables (si elles existent)...")
+        await conn.run_sync(Base.metadata.drop_all)
+        print("Création de la nouvelle table 'EmotionTable'...")
+        await conn.run_sync(Base.metadata.create_all)
+    print("Table 'EmotionTable' créée avec succès.")
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
