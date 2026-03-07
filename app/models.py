5b94ca9acec0a17fc532c9759abe0c5c1b64375b
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-03-07 07:32:22 +0000

    add 'created_at' & change 'score' to 'confidence'

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.sql import func 
 from app.database import Base
 
 class PredictionHistory(Base):
     __tablename__ = "EmotionTable"
-    id = Column(Integer, primary_key = True,index = True )
-    """ timestamp = Column(
-        DateTime(timezone=True), 
-        server_default = func.now()) """
     
-    filename = Column(String, index = True)
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, index=True)
     emotion = Column(String)
-    score = Column(Float)
+    
+    # 'score' to 'confidence' 
+    confidence = Column(Float) 
+    
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
